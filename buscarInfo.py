d72a852868dfd092ef4041a44a154d1cb2799298
Mikael <mikael_marceniuk@hotmail.com.br>
2020-10-31 14:30:51 -0300

    .Criado pesquisa e criacao dos deputados

--- buscarInfo.py
+++ buscarInfo.py
@@ -1,12 +1,9 @@
-import os
 import requests
 import json
 import xlsxwriter 
 
-os.system('cls')
-
 def fetchAndCreatePartidoData():
-    print('Buscando dados...')
+    print('Buscando dados dos partidos...')
 
     #Fetching
     resp = requests.get('https://dadosabertos.camara.leg.br/api/v2/partidos?itens=1000')
@@ -22,19 +19,17 @@ def fetchAndCreatePartidoData():
         partidosLst[i]['lider'] = partidoInfoDct.get('dados').get('status').get('lider').get('nome')
         partidosLst[i]['uf'] = partidoInfoDct.get('dados').get('status').get('lider').get('uf')
         partidosLst[i]['totalMembros'] = partidoInfoDct.get('dados').get('status').get('totalMembros')
-        del partidosLst[i]['id']
-        del partidosLst[i]['uri']
 
     print('Criando arquivo Excel...')
     #Creating
     workbook = xlsxwriter.Workbook('partidos.xlsx')
     worksheet = workbook.add_worksheet()
 
-    worksheet.write('A1', 'Sigla')
-    worksheet.write('B1', 'Nome')
-    worksheet.write('C1', 'Lider')
-    worksheet.write('D1', 'Uf')
-    worksheet.write('E1', 'Total de Membros')
+    worksheet.write('A1', 'sigla')
+    worksheet.write('B1', 'nome')
+    worksheet.write('C1', 'lider')
+    worksheet.write('D1', 'uf')
+    worksheet.write('E1', 'totalMembros')
 
     for i in range(len(partidosLst)):
         worksheet.write('A{}'.format(i + 2), partidosLst[i].get('sigla'))
@@ -46,4 +41,55 @@ def fetchAndCreatePartidoData():
     workbook.close()
     print('Criado arquivo excel "partidos.xlsx" com sucesso!')
 
-fetchAndCreatePartidoData()
+def fetchAndCreateDeputadosData():
+    print('Buscando dados dos deputados...')
+
+    #Fetching
+    resp = requests.get('https://dadosabertos.camara.leg.br/api/v2/deputados?itens=1')
+    deputadosDct = json.loads(resp.text)
+    deputadosLst = deputadosDct.get('dados')
+
+    #Pega mais info do partido
+    #e adiciona na list
+    for i in range(len(deputadosLst)):
+        resp = requests.get(deputadosLst[i].get('uri'))
+        deputadosInfoDct = json.loads(resp.text)
+
+        deputadosLst[i]['nome'] = deputadosInfoDct.get('dados').get('nomeCivil')
+        deputadosLst[i]['cpf'] = deputadosInfoDct.get('dados').get('cpf')
+        deputadosLst[i]['email'] = deputadosInfoDct.get('dados').get('email')
+        deputadosLst[i]['sexo'] = deputadosInfoDct.get('dados').get('sexo')
+        deputadosLst[i]['dataNascimento'] = deputadosInfoDct.get('dados').get('dataNascimento')
+        deputadosLst[i]['dataFalescimento'] = deputadosInfoDct.get('dados').get('dataFalecimento')
+        deputadosLst[i]['escolaridade'] = deputadosInfoDct.get('dados').get('escolaridade')
+
+    print('Criando arquivo Excel...')
+    #Creating
+    workbook = xlsxwriter.Workbook('deputados.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write('A1', 'nomeCivil')
+    worksheet.write('B1', 'cpf')
+    worksheet.write('C1', 'email')
+    worksheet.write('D1', 'sexo')
+    worksheet.write('E1', 'dataNascimento')
+    worksheet.write('F1', 'dataFalescimento')
+    worksheet.write('G1', 'escolaridade')
+    worksheet.write('H1', 'siglaPartido')
+    worksheet.write('I1', 'siglaUf')
+    worksheet.write('J1', 'urlFoto')
+
+    for i in range(len(deputadosLst)):
+        worksheet.write('A{}'.format(i + 2), deputadosLst[i].get('nome'))
+        worksheet.write('B{}'.format(i + 2), deputadosLst[i].get('cpf'))
+        worksheet.write('C{}'.format(i + 2), deputadosLst[i].get('email'))
+        worksheet.write('D{}'.format(i + 2), deputadosLst[i].get('sexo'))
+        worksheet.write('E{}'.format(i + 2), deputadosLst[i].get('dataNascimento'))
+        worksheet.write('F{}'.format(i + 2), deputadosLst[i].get('dataFalescimento'))
+        worksheet.write('G{}'.format(i + 2), deputadosLst[i].get('escolaridade'))
+        worksheet.write('H{}'.format(i + 2), deputadosLst[i].get('siglaPartido'))
+        worksheet.write('I{}'.format(i + 2), deputadosLst[i].get('siglaUf'))
+        worksheet.write('J{}'.format(i + 2), deputadosLst[i].get('urlFoto'))
+
+    workbook.close()
+    print('Criado arquivo excel "deputados.xlsx" com sucesso!')
