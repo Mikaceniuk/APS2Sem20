8b5018bab6e4fbaa864be5c5b3fc571565fa8b29
Mikael Marceniuk <mikael_marceniuk@hotmail.com.br>
2020-11-04 19:26:01 -0300

    Adicionado ordenacao

--- converterInfo.py
+++ converterInfo.py
@@ -1,6 +1,6 @@
 from openpyxl import load_workbook
 
-def converterExcelParaTxt(nomeExcel):
+def converterExcelParaTxt(nomeExcel, nomeCabecalho):
     print('Convertendo {}'.format(nomeExcel))
 
     path = '{}.xlsx'.format(nomeExcel)
@@ -8,24 +8,53 @@ def converterExcelParaTxt(nomeExcel):
     f = open('{}.txt'.format(nomeExcel), 'w+')
     sheet_obj = wb_obj.active
 
-    #Insere os valores
+    cabecalhos = []
+    valores = []
+
+    #Pega o cabeçalho e valor de cada linha
     for row in range(sheet_obj.max_row + 1):
+        valorLinha = {}
         for column in range(1, sheet_obj.max_column + 1):
             if(row > 0):
-                valor = sheet_obj.cell(row = row, column = column).value
-
-                #Cabeçalho
+                valorCelula = sheet_obj.cell(row = row, column = column).value
                 if(row == 1):
+                    cabecalhos.append(valorCelula)
                     if(column != sheet_obj.max_column):
-                        f.writelines('{} | '.format(valor))
+                        f.writelines('{} | '.format(valorCelula))
                     else:
-                        f.writelines('{} \n'.format(valor))
-                #Data
+                        f.writelines('{} \n'.format(valorCelula))
                 elif(row > 1):
-                    if(column != sheet_obj.max_column):
-                        f.writelines('{} | '.format(valor))
-                    else:
-                        f.writelines('{} \n'.format(valor))
+                    valorLinha['{}'.format(cabecalhos[column - 1])] = valorCelula                    
+        if(row > 1):
+            valores.append(valorLinha)
+
+    #Ordenar
+    indexCab = cabecalhos.index(nomeCabecalho)
+    for i in range(len(valores)):
+        if(i > 0):
+            vlrLinhaAtual = valores[i]
+            vlrLinhaAnterior = valores[i - 1]
+
+            while int(vlrLinhaAnterior.get(nomeCabecalho)) > int(vlrLinhaAtual.get(nomeCabecalho)):
+                indexLinhaAnt = valores.index(vlrLinhaAtual)
+                valores[valores.index(vlrLinhaAnterior)] = valores[valores.index(vlrLinhaAtual)]
+                valores[indexLinhaAnt] = vlrLinhaAnterior
+
+                nvIndex = valores.index(vlrLinhaAtual)
+                if(nvIndex == 0):
+                    i = 0
+                    break
+                vlrLinhaAtual = valores[nvIndex]
+                vlrLinhaAnterior = valores[nvIndex - 1]
+
+    #Escreve
+    for i in range(len(valores)):
+        for x in cabecalhos:
+            if(cabecalhos.index(x) != len(cabecalhos) - 1):
+                f.writelines('{} | '.format(valores[i][x]))
+            else:
+                f.writelines('{} \n'.format(valores[i][x]))
+
     f.close()
 
     print('Convertido {} com sucesso!'.format(nomeExcel))
--- main.py
+++ main.py
@@ -8,14 +8,14 @@ print('Verificando dados...')
 sleep(2)
 
 try:
-    converterInfo.converterExcelParaTxt('partidos')
+    converterInfo.converterExcelParaTxt('partidos', 'totalMembros')
 except OSError:
     print('Arquivo partidos não foi encontrado!')
     sleep(1)
     buscarInfo.fetchAndCreatePartidoData()
 
 try:
-    converterInfo.converterExcelParaTxt('deputados')
+    converterInfo.converterExcelParaTxt('deputados', 'cpf')
 except OSError:
     print('Arquivo deputados não foi encontrado!')
     sleep(1)
@@ -32,13 +32,13 @@ opt = int(input('''Deseja converter quais arquivos para TXT?
 
 if opt == 1:
     print('Você selecionou converter arquivo Partidos!')
-    converterInfo.converterExcelParaTxt('partidos')
+    converterInfo.converterExcelParaTxt('partidos', 'totalMembros')
 elif opt == 2:
     print('Você selecionou converter arquivo Deputados!')
-    converterInfo.converterExcelParaTxt('deputados')
+    converterInfo.converterExcelParaTxt('deputados', 'cpf')
 elif opt == 3:
     print('Você selecionou converter os dois arquivos!')
-    converterInfo.converterExcelParaTxt('partidos')
-    converterInfo.converterExcelParaTxt('deputados')
+    converterInfo.converterExcelParaTxt('partidos', 'totalMembros')
+    converterInfo.converterExcelParaTxt('deputados', 'cpf')
 else:
     print('Opção inválida, tente novamente!')
